de64135e7d68cae8c8f5dd987a5381112dad6e4f
SnaiperLoD <snaiperlod19@gmail.com>
2018-10-07 23:09:45 +0300

    99%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+
+  let max = 1;
+  let counter = 1;
+  const n=array.length
+
+  for (let i = 1, l = array.length; i < n; ++i)
+   {
+      if (array[i] === array[i - 1]) continue;
+
+      if (array[i] - 1 === array[i - 1]) 
+      {
+          counter++;
+      } 
+      else 
+      {
+          buff = array[i] - i;
+          counter = 1;
+      }
+
+      max = Math.max(counter, max);
+  }
+
+  if (n==0)
+  return 0;
+
+  return max;
+};
